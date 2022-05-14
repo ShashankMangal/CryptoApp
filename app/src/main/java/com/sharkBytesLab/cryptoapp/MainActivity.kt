69d5cc11822cda9e5341b2c8a6e5ecf1b33f48d1
ShashankMangal <shashankmangal10@gmail.com>
2022-05-14 08:29:01 +0530

    main activity layout ready

--- app/src/main/java/com/sharkBytesLab/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/sharkBytesLab/cryptoapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.sharkBytesLab.cryptoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
+import androidx.navigation.fragment.findNavController
+import com.sharkBytesLab.cryptoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+        val navController = navHostFragment!!.findNavController()
+
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.bottom_nav_menu)
+        binding.bottomBar.setupWithNavController(popupMenu.menu, navController)
     }
 }
