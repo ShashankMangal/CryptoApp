9a15fd6cd0ccad61f4a0da86132da58495b850cd
ShashankMangal <shashankmangal10@gmail.com>
2022-05-16 10:26:05 +0530

    top crypto view ready

---
+++ app/src/main/java/com/sharkBytesLab/cryptoapp/Adapters/TopMarketAdapter.kt
@@ -0,0 +1,53 @@
+package com.sharkBytesLab.cryptoapp.Adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.sharkBytesLab.cryptoapp.Models.CryptoCurrency
+import com.sharkBytesLab.cryptoapp.R
+import com.sharkBytesLab.cryptoapp.databinding.TopCurrencyLayoutBinding
+
+
+class TopMarketAdapter(var context : Context, val list : List<CryptoCurrency>) : RecyclerView.Adapter<TopMarketAdapter.TopMarketViewHolder>()
+{
+
+    inner class TopMarketViewHolder(view : View) : RecyclerView.ViewHolder(view)
+    {
+        var binding = TopCurrencyLayoutBinding.bind(view)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopMarketViewHolder {
+        return TopMarketViewHolder(LayoutInflater.from(context).inflate(R.layout.top_currency_layout, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: TopMarketViewHolder, position: Int) {
+        val item = list[position]
+
+        holder.binding.topCurrencyNameTextView.text = item.name
+
+        Glide.with(context).load("https://s2.coinmarketcap.com/static/img/coins/64x64/"+ item.id + ".png").thumbnail(Glide.with(context).load(R.drawable.spinner)).into(holder.binding.topCurrencyImageView)
+
+    if(item.quotes!![0].percentChange24h > 0)
+    {
+        holder.binding.topCurrencyChangeTextView.setTextColor(context.resources.getColor(R.color.green))
+        holder.binding.topCurrencyChangeTextView.text = "+${String.format("%.02f", item.quotes[0].percentChange24h)} %"
+    }else
+    {
+        holder.binding.topCurrencyChangeTextView.setTextColor(context.resources.getColor(R.color.red))
+        holder.binding.topCurrencyChangeTextView.text = "${String.format("%.02f", item.quotes[0].percentChange24h)} %"
+    }
+
+    }
+
+
+
+
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+}
